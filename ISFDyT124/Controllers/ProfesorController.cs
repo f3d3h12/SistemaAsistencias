c85bcc26c6f6fdcff96de8efd40160d6d92bcc19
IamGustav-Code <iamgustav.olivera@gmail.com>
2026-05-20 21:14:30 -0300

    Cracion de la nueva Rama Grupo-2

---
+++ ISFDyT124/Controllers/ProfesorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ISFDyT124.Controllers
+{
+    public class ProfesorController : Controller
+    {
+        rivate readonly InstitutoDbContext _context;
+
+        // Constantes del sistema para mapear con los IDs de la tabla ROLES
+        private const int ROL_DOCENTE_ID = 2; // ID del rol Docente en la base de datos
+        private const int ROL_ALUMNO_ID = 3;  // ID del rol Alumno en la base de datos
+
+        public ProfesorController(InstitutoDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
